e8da69edc78fd7f5d849aa198192b1d4ed262342
Mohamed-Ali-Tech <mohamedali.users@gmail.com>
2025-05-16 12:45:42 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+var typed1 = new Typed('#types-js', {
+    strings: ['Web Devloper.', ' Web Designer.'],
+    typeSpeed: 50,
+});
+
+// Scroll
+  ScrollReveal().reveal('.reveal', {
+    duration: 1000,     
+    distance: '50px',   
+    easing: 'ease-in-out',
+    origin: 'top', 
+    once: false
+  });
+
+ ScrollReveal().reveal('.border-bottom', {
+    duration: 1000,
+    distance: '50px',
+    easing: 'ease-in-out',
+    origin: 'left',
+    reset: true 
+  });
